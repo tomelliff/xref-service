57782b52f732180e4d20693323094b1f640d8316
Tom Elliff <tomelliff@gmail.com>
2017-04-13 15:05:20 +0100

    Basic script to create/delete a local DynamoDB table

--- table.py
+++ table.py
@@ -1,51 +1,102 @@
 #!/usr/bin/env python
+"""Create table locally and seed it with data
+
+Should be using Serverless or something else to build real DynamoDB tables in
+AWS.
+
+"""
+
+import argparse
+
 import boto3
 
 dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
 
-table = dynamodb.Table('xref-service')
+table_name = 'xref-service'
 
-def add_system_gsi(table, system):
-    table.update(
+def create_table(dynamodb, table_name):
+    dynamodb.create_table(
         AttributeDefinitions=[
             {
-                'AttributeName': system,
+                'AttributeName': 'global',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'm3',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'tp',
                 'AttributeType': 'S'
             }
         ],
-        GlobalSecondaryIndexUpdates=[
+        TableName=table_name,
+        KeySchema=[
             {
-                'Create': {
-                    'IndexName': '{}_system'.format(system),
-                    'KeySchema': [
-                        {
-                            'AttributeName': system,
-                            'KeyType': 'HASH'
-                        },
-                    ],
-                    'Projection': {
-                        'ProjectionType': 'ALL'
+                'AttributeName': 'global',
+                'KeyType': 'HASH'
+            }
+        ],
+        GlobalSecondaryIndexes=[
+            {
+                'IndexName': 'm3_system',
+                'KeySchema': [
+                    {
+                        'AttributeName': 'm3',
+                        'KeyType': 'HASH'
                     },
-                    'ProvisionedThroughput': {
-                        'ReadCapacityUnits': 5,
-                        'WriteCapacityUnits': 5
-                    }
+                ],
+                'Projection': {
+                    'ProjectionType': 'ALL'
+                },
+                'ProvisionedThroughput': {
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5
                 }
-            }
-        ]
-    )
-
-def delete_system_gsi(table, system):
-    table.update(
-        GlobalSecondaryIndexUpdates=[
+            },
             {
-                'Delete': {
-                    'IndexName': '{}_system'.format(system)
+                'IndexName': 'tp_system',
+                'KeySchema': [
+                    {
+                        'AttributeName': 'tp',
+                        'KeyType': 'HASH'
+                    },
+                ],
+                'Projection': {
+                    'ProjectionType': 'ALL'
+                },
+                'ProvisionedThroughput': {
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5
                 }
             }
-        ]
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
     )
 
-#add_system_gsi(table, 'tp')
 
-#delete_system_gsi(table, 'tp')
+table = dynamodb.Table(table_name)
+
+
+def delete_table(table):
+    table.delete()
+
+
+def add_test_data(table):
+    raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Manage a local DynamoDB table')
+    parser.add_argument('action',  choices=['create', 'delete', 'populate'])
+    args = parser.parse_args()
+    if args.action == 'create':
+        create_table(dynamodb, table_name)
+    elif args.action == 'delete':
+        delete_table(table)
+    elif args.action == 'populate':
+        add_test_data(table)
