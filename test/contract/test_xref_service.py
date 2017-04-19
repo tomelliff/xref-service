e27d125fe5d69cc29e7b6c842a316e9e78a3f6d7
Tom Elliff <tomelliff@gmail.com>
2017-04-19 13:55:31 +0100

    Add basic contract tests that works against dynamic API gateway endpoint url
    Serverless will deploy an API gateway with a unique name each time. This then gets passed to the contract test stage which will run tests against that endpoint.

--- test/contract/test_xref_service.py
+++ test/contract/test_xref_service.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 from __future__ import absolute_import
+import os
 import unittest
 
+import requests
 
-class TestGet(unittest.TestCase):
-    def test_get_by_global_id(self):
-        pass
+service_endpoint = os.environ['SERVICE_ENDPOINT']
+
+
+class TestPost(unittest.TestCase):
+    def test_create_new_global_id_returns_just_global_id(self):
+        new_global_id_path = 'xref-service/global'
+        url = service_endpoint + '/' + new_global_id_path
+        r = requests.post(url, data={})
+        self.assertEqual(type(r.json()), type({}))
+        self.assertTrue('global' in r.json())
+        self.assertEqual(len(r.json()), 1)
 
 
 if __name__ == '__main__':
