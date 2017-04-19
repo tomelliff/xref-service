a42ca4b4d13dc7305de189567b02c8e7d2b91f39
Tom Elliff <tomelliff@gmail.com>
2017-04-19 17:39:44 +0100

    Configurable service name in contract tests

--- test/contract/test_xref_service.py
+++ test/contract/test_xref_service.py
@@ -6,12 +6,13 @@ import unittest
 import requests
 
 service_endpoint = os.environ['SERVICE_ENDPOINT']
+service_name = os.environ.get('SERVICE_NAME', 'xref-service')
 
 
 class TestPost(unittest.TestCase):
     def test_create_new_global_id_returns_just_global_id(self):
-        new_global_id_path = 'xref-service/global'
-        url = service_endpoint + '/' + new_global_id_path
+        new_global_id_path = '/global'
+        url = service_endpoint + '/' + service_name + new_global_id_path
         r = requests.post(url, data={})
         self.assertEqual(type(r.json()), type({}))
         self.assertTrue('global' in r.json())
